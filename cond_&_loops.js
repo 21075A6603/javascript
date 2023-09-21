cca523141c975aba0473d963a15dc01e7d3dec58
azarMohd <azar.mohammed4u@gmail.com>
2023-09-21 22:46:59 +0530

    mdified

---
+++ cond_&_loops.js
@@ -0,0 +1,35 @@
+a=20
+b=30
+if(a>b){
+        console.log("a is big")
+
+}
+else{
+        console.log("b is big");
+}
+
+
+a=b
+
+if(a>b){
+        console.log("a is big");
+}
+else if(a==b)
+{
+        console.log("both are equal")
+}
+else{
+        console.log("b is big");
+}
+
+// loops 
+// forloop:
+for(var i=0;i<10;i++){
+        console.log(i+1)
+}
+console.log("do while loop:")
+do{
+        i=0
+        console.log(i)
+}while(i!=10)
+console.log("hi")
---
+++ functions.js
@@ -0,0 +1,5 @@
+function add(a,b){
+        return a+b
+}
+var x=add(10,20)
+console.log("hi hello",x)
