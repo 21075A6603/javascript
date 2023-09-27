ee8ce7dbc1aaed7c52a8c62a88c795a9c725e2a5
azarMohd <azar.mohammed4u@gmail.com>
2023-09-27 12:33:18 +0530

    interactiveness

---
+++ dom_adding_name.js
@@ -0,0 +1,12 @@
+let input=document.querySelector(".name")
+let password=document.querySelector('.pwd')
+let submit=document.querySelector('.btn1')
+
+submit.addEventListener("click",()=>{
+        console.log(input.value)
+        console.log(password.value)
+})
+
+
+
+
